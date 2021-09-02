e598500cfcba9ec0108ea47002f0d167867d894f
Sandydht <sandydwi75@gmail.com>
2021-09-02 08:14:02 +0700

    Compress image and add function for expand button on table

--- js/script.js
+++ js/script.js
@@ -1,10 +1,14 @@
-const button = document.getElementsByClassName('mt-table-expand-button');
+/** @format */
 
-for (let i = 0; i < button.length; i++) {
-  button[i].addEventListener('click', () => {
-    let panel = document.querySelectorAll(`.mt-table-panel-expand-${i}`);
-    panel.forEach(elm => {
-      elm.classList.toggle('open');
-    })
-  })
-}
+document.addEventListener("DOMContentLoaded", () => {
+  const button = document.getElementsByClassName("mt-table-expand-button");
+
+  for (let i = 0; i < button.length; i++) {
+    button[i].addEventListener("click", () => {
+      let panel = document.querySelectorAll(`.mt-table-panel-expand-${i}`);
+      panel.forEach((elm) => {
+        elm.classList.toggle("open");
+      });
+    });
+  }
+});
